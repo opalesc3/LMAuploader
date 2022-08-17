26d8824ad5f2dee1f9bbeff04ce85caaeba78a65
opalesc3 <111399020+opalesc3@users.noreply.github.com>
2022-08-16 22:56:55 -0500

    Autofill name with tag title if blank

--- Mac/rename.sh
+++ Mac/rename.sh
@@ -11,10 +11,14 @@ IFS=$(echo -en "\n\b")
 for file in ${folder}/*${format}
 do
 	echo -e "\n${file}"
-	echo -n "Current tag: "
-	ffprobe $file -show_entries format_tags=title -of compact=p=0:nk=1 -v 0
+	taggedTitle=$(ffprobe $file -show_entries format_tags=title -of compact=p=0:nk=1 -v 0)
+	echo "Current tag: ${taggedTitle}"
 	read -p "Track number: " num
 	read -p "Track name: " name
+	if [ ${#name} == 0 ]
+	then
+		name=$taggedTitle
+	fi
 	name=$(echo "${name}" | sed 's/ //g')
 	newName="${id}t_${num}${name}${format}"
 	read -p "Renaming to ${newName}"
