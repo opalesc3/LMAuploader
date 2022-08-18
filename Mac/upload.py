a18dfbabcde91218b3fb3835926e6e25db62f741
opalesc3 <111399020+opalesc3@users.noreply.github.com>
2022-08-17 21:56:40 -0500

    Update upload.py
    Reorganized code. Wrote function to rename files. Added input validation

--- Mac/upload.py
+++ Mac/upload.py
@@ -1,63 +1,126 @@
 from internetarchive import upload
 from internetarchive import search_items
-from subprocess import call
+import music_tag
+import datetime
+import os
 
-# user required input
-print("All fields marked with an * are required. Press enter to skip non-essential fields")
-dir = str(input("Folder name*: "))
-format = str(input("File format*: "))
-date = str(input("Date*: "))
-venue = str(input("Venue*: "))
-coverage = str(input("Location*: "))
-taper = str(input("Taper: "))
-transferer = str(input("Transferer: "))
-source = str(input("Source: "))
-lineage = str(input("Lineage: "))
+def main():
+    # user required input
+    print("All fields marked with an * are required. Press enter to skip non-essential fields")
 
-# generate other metadata
-title = "Animal Collective Live at " + venue + " on " + date
-year = date[:4]
-id = "acollective" + date
+    directory = emptyInputCheck("Folder name*: ")
+    while(os.path.isdir(directory) is False):
+        print("Invalid folder.")
+        directory = str(input("Folder name*: "))
+
+    date = emptyInputCheck("Date*: ")
+    dateValid = False
+    while(dateValid is False):
+        try:
+            datetime.datetime.strptime(date, '%Y-%m-%d')
+            dateValid = True
+        except ValueError:
+            print("Invalid date. Please enter in yyyy-mm-dd format.")
+
+    venue = emptyInputCheck("Venue*: ")
+    coverage = str(input("Location: "))
+    taper = str(input("Taper: "))
+    transferer = str(input("Transferer: "))
+    source = str(input("Source: "))
+    lineage = str(input("Lineage: "))
+
+    # generate other metadata
+    title = "Animal Collective Live at " + venue + " on " + date
+    year = date[:4]
+    identifier = "acollective" + date
+
+    identifier = idCheck(identifier)
+    print("Successfully generated identifier " + identifier + "!")
+
+    re = str(input("Rename files? (y/n): "))
+    if re == "y":
+        rename(directory, identifier)
+
+    description = genInfo(directory, identifier, date, venue, coverage)
+    uploadFiles(directory, identifier, title, date, year, venue, coverage, description, taper, transferer, source, lineage)
+
 
 # check if identifier is taken
-search = search_items("identifier:" + id)
-while len(search) > 0:
-	suffix = str(input("Identifier [" + id + "] is taken, please add a suffix: "))
-	id = id + suffix
-	search = search_items("identifier:" + id)
-print("Successfully generated identifier " + id + "!")
+def idCheck(identifier):
+    search = search_items("identifier:" + identifier)
+    while len(search) > 0:
+        suffix = emptyInputCheck("Identifier [" + identifier + "] is taken, please add a suffix: ")
+        identifier = identifier + suffix
+        search = search_items("identifier:" + identifier)
+    return str(identifier)
 
-# call rename.sh [directory name] [date] [format] [id]
-rename = str(input("Rename files? (y/n): "))
-if rename == "y":
-	call(["./rename.sh", dir, date, format, id])
+
+# Rename files
+def rename(directory, identifier):
+    for file in os.listdir(directory):
+        filename = str(os.fsdecode(file))
+        print("\n" + filename)
+        ext = os.path.splitext(file)[-1].lower()
+        tags = music_tag.load_file(directory + "/" + filename)
+
+        print("Current track number and track title: " + str(tags['tracknumber']).zfill(2) + " " + str(tags['title']))
+        print("Leave blank to use current values.")
+        num = str(input("Track number: "))
+        title = str(input("Track title: ")).replace(" ", "")
+        if len(num) == 0:
+            num = str(tags['tracknumber']).zfill(2)
+        if len(title) == 0:
+            title = str(tags['title']).replace(" ", "")
+        title = "".join([i if i not in "\/:*?<>|" else "-" for i in title])
+        newName = identifier + "t_" + num + title + ext
+        input("Renaming file to " + newName)
+        os.rename(directory + "/" + filename, directory + "/" + newName)
+
 
 # generate info file and description
-descriptionHTML = ""
-customDescription = input("Please enter a description (one line only).\n")
-info = open(dir + "/" + id + "info.txt", "x")
-info = open(dir + "/" + id + "info.txt", "w")
-info.write("Animal Collective\n" + date + "\n" + venue + "\n" + coverage + "\n\n")
-info.write(customDescription + "\n\n")
-descriptionHTML = descriptionHTML + "<div>" + customDescription + "</div><div><br /></div>"
+def genInfo(directory, identifier, date, venue, coverage):
+    descriptionHTML = ""
+    customDescription = input("Please enter a description (one line only).\n")
+    infoFile = open(str(directory) + "/" + str(identifier) + "info.txt", "x")
+    infoFile = open(str(directory) + "/" + str(identifier) + "info.txt", "w")
+    infoFile.write("Animal Collective\n" + date + "\n" + venue + "\n" + coverage + "\n\n")
+    infoFile.write(customDescription + "\n\n")
+    descriptionHTML = descriptionHTML + "<div>" + customDescription + "</div><div><br /></div>"
 
-numSongs = int(input("\nSetlist length: "))
-print("Paste setlist")
-for i in range(numSongs):
-	song = str(input())
-	info.write(song + "\n")
-	descriptionHTML = descriptionHTML + "<div>" + song + "</div>"
+    numSongs = int(emptyInputCheck("\nSetlist length: "))
+    print("Paste setlist")
+    for i in range(numSongs):
+        song = emptyInputCheck()
+        infoFile.write(song + "\n")
+        descriptionHTML = descriptionHTML + "<div>" + song + "</div>"
 
-info.close()
+    infoFile.close()
+    return descriptionHTML
+
 
 # upload
-md = {'mediatype': 'etree', 'collection': 'etree', 'collection': 'AnimalCollective', 'creator': 'Animal Collective', 'subject': 'Live concert', 'title': title, 'year': year, 'type': 'sound', 'venue': venue, 'date': date, 'coverage': coverage, 'description': descriptionHTML, 'taper': taper, 'transferer': transferer, 'source': source, 'lineage': lineage}
+def uploadFiles(directory, identifier, title, date, year, venue, coverage, description, taper, transferer, source,
+                lineage):
+    md = {'mediatype': 'etree', 'collection': 'etree', 'collection': 'AnimalCollective', 'creator': 'Animal Collective',
+          'subject': 'Live concert', 'title': title, 'year': year, 'type': 'sound', 'venue': venue, 'date': date,
+          'coverage': coverage, 'description': description, 'taper': taper, 'transferer': transferer, 'source': source,
+          'lineage': lineage}
 
-r = upload(id, dir + "/", metadata=md)
-status = str(r[0].status_code)
+    r = upload(identifier, directory + "/", metadata=md)
+    status = str(r[0].status_code)
 
-if status == "200":
-	print("Success!")
-	print("https://archive.org/details/" + id)
-else:
-	print("Status code: " + status)
+    if status == "200":
+        print("Success!")
+        print("https://archive.org/details/" + identifier)
+    else:
+        print("Status code: " + status)
+
+def emptyInputCheck(prompt=""):
+    i = str(input(prompt))
+    while(len(i) == 0):
+        print("This field cannot be left empty.")
+        i = str(input(prompt))
+    return i
+
+if __name__ == "__main__":
+    main()
